4582d1f37215182abd53e9709fda749925a3e875
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2026-03-15 21:37:38 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.mynametithi;
+public class Main {
+public static void main(String[] args) {
+    // int[] marks=new int[3];
+    // marks[0]=97;
+    // marks[1]=94;
+    // marks[2]=89;
+    // System.out.println(marks[0]);
+    // Arrays.sort(marks);
+    // System.out.println(marks[0]);
+    // System.out.println(marks[1]);
+    // System.out.println(marks[2]);
+    int[][] marks2nd={{94,89,23},{67,78,76}};
+    System.out.println(marks2nd[0][2]);
+    // constant
+    final float PI=3.14f;
+}
+    
+    
+
+}
