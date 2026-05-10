98559a0601a51cbab228c259a3c90e8f5ba492aa
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2026-05-10 11:56:13 +0530

    Add files via upload

---
+++ hashMap.java
@@ -0,0 +1,12 @@
+
+import java.util.HashMap;
+
+public class hashMap {
+public static void main(String[] args) {
+  HashMap<String, Integer> map = new HashMap<>();
+  map.put("India",120);
+    map.put("China",150);
+    map.put("USA",50);
+    System.out.println(map);
+ }
+}
