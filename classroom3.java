2a075b4c4ac367f3b46b70afe83995b1f4219160
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2026-05-25 23:18:52 +0530

    Add files via upload

---
+++ classroom3.java
@@ -0,0 +1,64 @@
+import java.util.*;
+//IF the graph are sum of disconnected components
+
+public class classroom3 {
+    static class Edge{
+int src;
+int dest;
+public Edge(int s,int d){
+    this.src=s;
+    this.dest=d;
+}
+    }
+    public static void createGraph(ArrayList<Edge> graph[] ){
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+        graph[6].add(new Edge(6, 5));
+}
+public static void bfs(ArrayList<Edge>graph[],int V,boolean visited[],int start){
+    Queue<Integer> q=new LinkedList<>();
+   
+    q.add(start);
+    while(!q.isEmpty()){
+int curr=q.remove();
+if(!visited[curr]){
+    System.out.print(curr+" ");
+    visited[curr]=true;
+    for(int i=0;i<graph[curr].size();i++){
+        Edge e=graph[curr].get(i);
+        q.add(e.dest);
+    }
+}
+}
+}
+public static void main(String[] args) {   //0(V+E)
+    int V=7;
+    ArrayList<Edge>graph[]=new ArrayList[V];
+    createGraph(graph);
+     boolean[] visited=new boolean[V];
+     for(int i=0;i<V;i++){
+        if(visited[i]==false){
+  bfs(graph, V, visited, i); 
+        }
+     }
+  
+    System.out.println(); 
+}
+}
+
