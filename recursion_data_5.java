932fe99a431245cdca8701406daedd66463a1e3c
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2026-06-04 20:25:36 +0530

    Add files via upload

---
+++ recursion_data_5.java
@@ -0,0 +1,21 @@
+//Print fibonacci seqence till nth term:
+public class recursion_data_5 {
+    public static void printFib(int a, int b, int n) {
+        if (n == 0) {
+            return;
+        }
+        int c = a + b;
+        System.out.print(c + " ");
+        printFib(b, c, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+        int n = 7;
+        printFib(a, b, n - 2);
+
+    }
+}
