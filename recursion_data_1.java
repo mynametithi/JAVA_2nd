a08624ba2ac7d7b83b5c45b0bf7a90b68d563eab
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2026-06-04 20:23:01 +0530

    Add files via upload

---
+++ recursion_data_1.java
@@ -0,0 +1,15 @@
+//print 5 to 1 number using recursive functon:
+public class recursion_data_1 {
+    public static void printNum(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        printNum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        printNum(n);// n=5
+    }
+}
