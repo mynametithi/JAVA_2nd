2aab66ead0cfe3513db5bae50d7cf787e82aa184
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2026-06-07 18:37:34 +0530

    Add files via upload

--- recursion_data_4.java
+++ recursion_data_4.java
@@ -1,15 +1,27 @@
-//Print n number of factorial:
+//Move all x to the end of the string:
 public class recursion_data_4 {
+    public static void moveAllX(String str, int idx, int count, String newString) {
+        if (idx == str.length()) {
+            for (int i = 0; i < count; i++) {
+                newString += 'x';
+            }
+            System.out.println(newString);
+            return;
 
-    public static int factorial(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        }
+        char currChar = str.charAt(idx);
+        if (currChar == 'x') {
+            count++;
+            moveAllX(str, idx + 1, count, newString);
+        } else {
+            newString += currChar; // newString = newString + currChar;
+            moveAllX(str, idx + 1, count, newString);
         }
 
-        return n * factorial(n - 1);
     }
 
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        String str = "axbxcxdxex";
+        moveAllX(str, 0, 0, " ");
     }
 }
