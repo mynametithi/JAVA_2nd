f2ff2b0b217b519ad1c60a905d26838142887c71
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2026-06-12 22:57:35 +0530

    Add files via upload

---
+++ data_sorting_1.java
@@ -0,0 +1,22 @@
+public class data_sorting_1 {
+    public static void printArray(int arr[]){
+for(int i=0;i<arr.length;i++){
+    System.out.print(arr[i]+" ");
+}
+    }
+public static void main(String[] args) {
+       int arr[]={2,4,1,7,3,9,6,5};
+       //bubble sort
+       for(int i=0;i<arr.length-1;i++){
+        for(int j=0;j<arr.length-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                //swap
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+       }
+       printArray(arr);
+}
+}
